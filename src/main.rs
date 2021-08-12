99849e95ea13a703cb4c05dd1c11b4eac96a2967
Simranjeet Singh <smrnjeet222@gmail.com>
2021-08-13 01:03:12 +0530

    complete

--- src/main.rs
+++ src/main.rs
@@ -1,43 +1,22 @@
 use std::env;
-use std::fs;
 use std::process;
-use std::error::Error;
+
+use minigrep::Config;
+
 
 fn main() {
     let args: Vec<String> = env::args().collect();
 
     let config = Config::new(&args).unwrap_or_else(|err| {
-        println!("Problem parsing arguments: {}", err);
+        eprintln!("Problem parsing arguments: {}", err);
         process::exit(1);
     });
 
-    if let Err(e) = run(config) {
-        println!("Problem running: {}", e);
+    if let Err(e) = minigrep::run(config) {
+        eprintln!("Problem running: {}", e);
         process::exit(1);
     }
 
 }
 
-fn run(config: Config) -> Result<(), Box<dyn Error>> {
-    let content = fs::read_to_string(config.filename)?;
-    dbg!(content);
 
-    Ok(())
-}
-
-#[derive(Debug)]
-struct Config {
-    query: String,
-    filename: String,
-}
-
-impl Config {
-    fn new(args: &[String]) -> Result<Self, &str> {
-        if args.len() < 3 {
-            return Err("Not enough arguments");
-        }
-        let query = args[1].clone();
-        let filename = args[2].clone();
-        Ok(Self { query, filename })
-    }
-}
